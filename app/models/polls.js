31751543a51e828fdca41db8d20c4125d5c3d6be
maksad <d.maksad@gmail.com>
2017-07-26 22:36:44 +0300

    Refactor polls model

--- app/models/polls.js
+++ app/models/polls.js
@@ -9,15 +9,14 @@ var Poll = new Schema({
     ref: 'User'
   },
   title: String,
-  options: [
-    {
-      title: String,
-      voters: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      }]
+  options: [{title: String}],
+  voters: [{
+    optionId: String,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     }
-  ]
+  }]
 });
 
 module.exports = mongoose.model('Poll', Poll);
